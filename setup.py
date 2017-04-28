6a5c9ccc8ab57933788e1871a82cfc9dd49796ab
Henry Hund <henryhund@gmail.com>
2017-04-28 17:21:02 -0400

    updated config and setup

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@
 
 from setuptools import setup, find_packages
 
-setup(name='tap-github',
-      version='0.2.0',
-      description='Singer.io tap for extracting data from the GitHub API',
-      author='Stitch',
+setup(name='tap-pipedrive',
+      version='0.1.0',
+      description='Singer.io tap for extracting data from the Pipedrive API',
+      author='Henry Hund (henryhund@gmail.com)',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_github'],
+      py_modules=['tap_pipedrive'],
       install_requires=['singer-python==1.6.0a2',
                         'requests==2.13.0'],
       entry_points='''
           [console_scripts]
-          tap-github=tap_github:main
+          tap-pipedrive=tap_pipedrive:main
       ''',
-      packages=['tap_github'],
+      packages=['tap_pipedrive'],
       package_data = {
-          'tap_github': [
-              'commits.json',
-              'issues.json'
+          'tap_pipedrive': [
+              # 'commits.json',
+              # 'issues.json'
               ]
           }
 )
