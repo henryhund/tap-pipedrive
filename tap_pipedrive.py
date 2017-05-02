efab4764beb4c32998c7fc5e4fda44a5d7055f42
Henry Hund <henryhund@gmail.com>
2017-05-02 10:33:12 -0400

    pulling all deals

--- tap_pipedrive.py
+++ tap_pipedrive.py
@@ -3,120 +3,112 @@ import requests
 import singer
 import json
 import os
-import singer.stats
+# import singer.stats
 
 session = requests.Session()
 logger = singer.get_logger()
 
-BASE_URL = "https://api.pipedrive.com/v1/"
+parser = argparse.ArgumentParser()
 
-endpoints = {
-    deals =  "/deals",
-    deal = "/deals/{id}",
-    deal_flow = "/deals/{id}/flow"
+parser.add_argument(
+    '-c', '--config', help='Config file', required=True)
 
-}
+args = parser.parse_args()
 
-access_token = config['access_token']
+with open(args.config) as config_file:
+    config = json.load(config_file)
 
-auth = "?api_token = " + 
+api_token = config['api-token']
 
-def get_url(endpoint, **kwargs):
-    BASE_URL + endpoints[endpoint].format(**kwargs) + auth + "&start={start}&limit={limit}&sort={sort}".format(**kwargs) 
+auth = "?api_token=" + api_token
+
 
 
 def authed_get(source, url):
-    with singer.stats.Timer(source=source) as stats:
-        resp = session.request(method='get', url=url)
-        stats.http_status_code = resp.status_code
-        return resp
+    # with singer.stats.Timer(source=source) as stats:
+    resp = session.request(method='get', url=url)
+    # stats.http_status_code = resp.status_code
+    return resp
 
-# to do: figure out paging
-
-# def authed_get_all_pages(source, url):
-#     while True:
-#         r = authed_get(source, url)
-#         yield r
-#         if 'next' in r.links:
-#             url = r.links['next']['url']
-#         else:
-#             break
+def authed_get_all_pages(source, url):
+    while True:
+        r = authed_get(source, url)
+        yield r
+        if r.json()['additional_data']['pagination']['more_items_in_collection']:
+            url = url +'&start='+str(r.json()['additional_data']['pagination']['next_start'])
+        else:
+            break
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
-# to do: schemas
+# TO DO: update schemas
 
-# def load_schemas():
-#     schemas = {}
+def load_schemas():
+    schemas = {}
 
-#     with open(get_abs_path('tap_github/commits.json')) as file:
-#         schemas['commits'] = json.load(file)
+    with open(get_abs_path('tap_github/commits.json')) as file:
+        schemas['commits'] = json.load(file)
 
-#     with open(get_abs_path('tap_github/issues.json')) as file:
-#         schemas['issues'] = json.load(file)
+    with open(get_abs_path('tap_github/issues.json')) as file:
+        schemas['issues'] = json.load(file)
 
-#     return schemas
+    return schemas
 
-def get_all_commits(repo_path, state):
-    if 'commits' in state and state['commits'] is not None:
-        query_string = '?since={}'.format(state['commits'])
+def get_all_deals(state):
+    if 'deals' in state and state['deals'] is not None:
+        query_string = '&sort=id%20DESC&limit=500'
     else:
-        query_string = ''
+        query_string = '&sort=id%20DESC&limit=500'
 
-    latest_commit_time = None
+    latest_deal_time = None
 
-    with singer.stats.Counter(source='commits') as stats:
-        for response in authed_get_all_pages('commits', 'https://api.github.com/repos/{}/commits{}'.format(repo_path, query_string)):
-            commits = response.json()
+    # with singer.stats.Counter(source='deals') as stats:
+    url = 'https://api.pipedrive.com/v1/deals{}{}'.format(auth, query_string)
+    deal_count = 0
+    for response in authed_get_all_pages('deals', url ):
+            deals = response.json()['data']
+            # logger.info('URL: %s ', url)
+            # logger.info('Deals: %s ', deals)
+            
+            for deal in deals:
+                # stats.add(record_count=1)
+                
+                deal_id = str(deal.pop('id', None))
+                deal_count = deal_count + 1
+                # if deal.get('update_time') >= state.get('deals'):
+                    
+                singer.write_record('deals', deal)
+                    
 
-            for commit in commits:
-                stats.add(record_count=1)
-                commit.pop('author', None)
-                commit.pop('committer', None)
+                # elif state['deals'] is None:
+                #     singer.write_records('deal', deals)
+                # else:
+                #     break
+            if not latest_deal_time:
+                latest_deal_time = deals[0]['update_time']
+    logger.info(deal_count)
 
-            singer.write_records('commits', commits)
-            if not latest_commit_time:
-                latest_commit_time = commits[0]['commit']['committer']['date']
-
-    state['commits'] = latest_commit_time
-    return state
-
-def get_all_issues(repo_path, state):
-    if 'issues' in state and state['issues'] is not None:
-        query_string = '&since={}'.format(state['issues'])
-    else:
-        query_string = ''
-
-    last_issue_time = None        
-    with singer.stats.Counter(source='issues') as stats:        
-        for response in authed_get_all_pages('issues', 'https://api.github.com/repos/{}/issues?sort=updated&direction=asc{}'.format(repo_path, query_string)):
-            issues = response.json()
-            stats.add(record_count=len(issues))
-            if len(issues) > 0:
-                last_issue_time = issues[-1]['updated_at']
-            singer.write_records('issues', issues)
-
-    state['issues'] = last_issue_time
+    state['deal'] = latest_deal_time
     return state
 
 def do_sync(config, state):
-    access_token = config['access_token']
-    repo_path = config['repository']
+    # access_token = config['access_token']
+    # repo_path = config['repository']
     # schemas = load_schemas()
     
-    session.headers.update({'authorization': 'token ' + access_token})
+    # session.headers.update({'authorization': 'token ' + access_token})
 
     if state:
-        logger.info('Replicating commits since %s from %s', state, repo_path)
+        logger.info('Replicating deals since %s ', state)
     else:
-        logger.info('Replicating all commits from %s', repo_path)
+        logger.info('Replicating deals ')
 
         
-    singer.write_schema('commits', schemas['commits'], 'sha')
-    singer.write_schema('issues', schemas['issues'], 'id')
-    state = get_all_commits(repo_path, state)
-    state = get_all_issues(repo_path, state)
+    # singer.write_schema('commits', schemas['commits'], 'sha')
+    # singer.write_schema('issues', schemas['issues'], 'id')
+    state = get_all_deals(state)
+    # state = get_all_issues(repo_path, state)
     singer.write_state(state)
 
 
@@ -135,7 +127,7 @@ def main():
         config = json.load(config_file)
 
     missing_keys = []
-    for key in ['access_token', 'repository']:
+    for key in ['api-token']:
         if key not in config:
             missing_keys += [key]
 
@@ -153,3 +145,4 @@ def main():
     
 if __name__ == '__main__':
     main()
+
